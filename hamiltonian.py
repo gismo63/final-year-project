22a4bae0ec8c64295449e9a1ccf1bad7dc95406c
Eoin <oconne19@tcd.ie>
2018-09-28 21:45:11 +0100

    Added magnetic field contribution and spin expectation value calculator

--- hamiltonian.py
+++ hamiltonian.py
@@ -35,21 +35,59 @@ def zz(i,j,n):
 
     return z_mat
 
-def hamiltonian(n,J): #calculates the Hamiltonian for the Heisenberg model
+def mag_field(n): #returns the sum over all S_iz which when multiplied by the field strength gives the contribution of the magnetic field to the hamiltonian
+    field = np.zeros((2**n,2**n))
+    for i in range(n):
+        order = []
+        for k in range(n):
+            if (k)==i:
+                order.append(s_z)
+            else:
+                order.append(identity)
+        z_mat = order[n-1]
+        for l in range(n-1):
+            z_mat = np.kron(order[n-l-2],z_mat)
+        field += z_mat
+    print field
+    return field
+
+def hamiltonian(n,J,B): #calculates the Hamiltonian for the Heisenberg model
     H=0
-    for j in range(len(J)):
+    for j in range(n):
         i=0
         while i<j: #only want to consider each interaction once and don't want i=j
             if J[i][j] != 0: #check if the spins interact at all
                 print i+1,j+1
-                H += (J[i][j])*(plusminus_minusplus(i,j,n)/(2.0) + zz(i,j,n)/(4.0))
-                print (J[i][j])*(plusminus_minusplus(i,j,n)/(2.0) + zz(i,j,n)/(4.0))
+                H += (J[i][j])*(plusminus_minusplus(i,j,n)/2. + zz(i,j,n)/4.)
+                #print (J[i][j])*(plusminus_minusplus(i,j,n)/2. + zz(i,j,n)/4.)
             i += 1
+    if B!=0:
+        H = np.add(H, B*mag_field(n)/2) #adds the magnetic field contribution to the hamiltonian, the factor of 1/2 is to account for the lack of this factor in the definition of s_z
 
     return H
 
 def eigen(matrix):
     return np.linalg.eigh(matrix) #calculates eigenvalues and eigenvectors of a hermitian matrix
+
+
+def expect_sz(n, g_state): #Calculates the expectation values of S_iz for each i
+    Sz = np.zeros(n)
+    for i in range(n):
+        order = []
+        for k in range(n):
+            if (k)==i:
+                order.append(s_z)
+            else:
+                order.append(identity)
+        z_mat = order[n-1]
+        for l in range(n-1):
+            z_mat = np.kron(order[n-l-2],z_mat)
+        Sz[i] = g_state.dot(z_mat.dot(g_state)) #computes <phi0|S_iz|phi0> where phi0 is the ground state eigenvector
+    return Sz
+
+
+
+
 
 
 
@@ -63,15 +101,32 @@ strength = 1 #overall multiplicative factor of interation strength
 
 #interaction strength where J[i][j] represents the strength of the interaction between particle i and particle j
 #J = strength*np.array([[0,1,0,0,1],[0,0,1,0,0],[0,0,0,1,0],[0,0,0,0,1],[0,0,0,0,0]]) #J for 5 electron chain with periodic boundary conditions
-J = strength*np.array([[0,1,0,1],[0,0,1,0],[0,0,0,1],[0,0,0,0]]) #J for 4 electron chain with periodic boundary conditions
+J = strength*np.array([[0,1,1,1],[0,0,1,1],[0,0,0,1],[0,0,0,0]]) #J for 4 electron chain with periodic boundary conditions
 #J = strength*np.array([[0,1,1],[0,0,1],[0,0,0]])
 #J = np.array([[0,1],[1,0]]) #J for 2 electron system
-H = hamiltonian(len(J),J)
+
+n = len(J)
+B = 0.5 #magnetic field strength
+
+H = hamiltonian(n,J,B)
 
 print H
 
 eigenvalues, eigenvectors = eigen(H)
 
-print eigenvalues
+eigenvalues = eigenvalues.round(10)
 
-#print eigenvectors
+print np.sort(eigenvalues)
+
+
+eigenvectors = eigenvectors.round(10)
+
+
+
+g_state = eigenvectors[:,0]
+
+print g_state
+
+Sz = expect_sz(n, g_state)/2
+
+print Sz.round(10)
